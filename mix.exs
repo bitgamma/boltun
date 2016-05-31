1759399baa88742824ee1d52ce9b7c74db1b2f2a
Adam Boas <adam.boas@gmail.com>
2016-05-31 10:36:24 +1000

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Boltun.Mixfile do
 
   def project do
     [app: :boltun,
-     version: "0.1.0-dev",
+     version: "0.1.1-dev",
      elixir: "~> 1.0",
      deps: deps,
      package: package,
